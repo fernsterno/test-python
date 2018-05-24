ebe8d6f953837e9b38a13a563344d6490a44e3d5
dev1 <admin@atgenes.com>
2018-05-24 16:30:29 +0700

    Update pyrebase

--- firebase/read.py
+++ firebase/read.py
@@ -1,8 +1,21 @@
-from firebase import firebase
+import pyrebase
 import time
-firebase = firebase.FirebaseApplication('https://test-f97f0.firebaseio.com', None)
-result = firebase.get('/users',None)
+
+config = {
+    "apiKey": "*****************",
+    "authDomain": "*****************",
+    "databaseURL": "*****************",
+    "storageBucket":"*****************"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+auth_user = auth.sign_in_with_email_and_password('*****************', '*****************')
+
+db = firebase.database()
+
 while True:
-    result = firebase.get('/users',None)
-    print(result)
+    users = db.child("users").get(auth_user['idToken'])
+    print(users.val())
     time.sleep(2)
